df23eedab4935d884e9e0aa0b762197c8c929b11
markmurimi <murimimg180@gmail.com>
2018-04-07 19:27:16 +0300

    modify changes

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,3 +3,10 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
+
+def creation_app(config_name):
+    app = Flask(__name__)
+
+    """Creating the configurations to run the app"""
+
+    app.config.from_object(config_options[config_name])
