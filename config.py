72a21287154b04ac6a6242e719c5d12063026942
markmurimi <murimimg180@gmail.com>
2018-04-08 10:37:19 +0300

    solving errors

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     """This is the config class for the whole app"""
     NEWS_API_KEY = 'a0105d92777949db8ce68f1b4952b724'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v1/sources/'
 
 class  DevConfig(Config):
     """This is the configuration class for the development stage of the app"""
