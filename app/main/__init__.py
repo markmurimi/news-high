7b8566dd44ef7469ea898f0c84f5bf03563ed517
markmurimi <murimimg180@gmail.com>
2018-04-08 09:20:24 +0300

    modified files

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,12 +1,3 @@
-from config import config_options 
-from flask import Flask
-from flask_bootstrap import Bootstrap
-
-bootstrap = Bootstrap()
-
-def creation_app(config_name):
-    app = Flask(__name__)
-
-    """Creating the configurations to run the app"""
-
-    app.config.from_object(config_options[config_name])
+from flask import Blueprint
+from . import views, errors
+main = Blueprint('main', __name__)
